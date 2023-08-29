9058c8905bbaa021a47dfff86fb971a5b6879643
rhildred <rhildred@gmail.com>
2023-08-29 19:04:55 +1000

    try with docs-build

---
+++ docs-build/assets/Header.stories-6f2a7ad4.js
@@ -0,0 +1,8 @@
+import{H as m}from"./Header-672333c9.js";import"./Button-c9607e11.js";import"./_commonjsHelpers-de833af9.js";import"./jsxRuntime.module-c37c4584.js";import"./preact.module-a8db4ca9.js";const l={title:"Example/Header",component:m,tags:["autodocs"],parameters:{layout:"fullscreen"},argTypes:{onLogin:{action:"onLogin"},onLogout:{action:"onLogout"},onCreateAccount:{action:"onCreateAccount"}}},e={args:{user:{name:"Jane Doe"}}},o={};var r,a,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: 'Jane Doe'
+    }
+  }
+}`,...(t=(a=e.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};var s,n,c;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(c=(n=o.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const L=["LoggedIn","LoggedOut"];export{e as LoggedIn,o as LoggedOut,L as __namedExportsOrder,l as default};
+//# sourceMappingURL=Header.stories-6f2a7ad4.js.map

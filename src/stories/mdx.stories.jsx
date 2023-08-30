e250e8b09cb3ba3feeffbd750132a671c71c9bfe
rhildred <rhildred@gmail.com>
2023-08-31 03:28:04 +1000

    add .mdx story

---
+++ src/stories/mdx.stories.jsx
@@ -0,0 +1,11 @@
+import Test from '../test.mdx';
+
+// More on how to set up stories at: https://storybook.js.org/docs/preact/writing-stories/introduction
+export default {
+  title: 'Example/Mdx with custom element',
+  component: Test,
+  tags: ['autodocs'],
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/preact/writing-stories/args
+export const Small = {};

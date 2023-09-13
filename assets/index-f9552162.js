81ba17abaac3062e7d67bba8b5d1f644141e8371
rhildred <rhildred@users.noreply.github.com>
2023-09-13 16:18:47 +0000

    deploy: 0b576cb7d4101568d41116686299290a2bcca039

---
+++ assets/index-f9552162.js
@@ -0,0 +1,2 @@
+import{M as m,a as p,u as a,w as i}from"./index-1cbf7c0a.js";import"./compat.module-f6b0595f.js";import"./preact.module-a8db4ca9.js";import"./hooks.module-1ffebabd.js";export{m as MDXContext,p as MDXProvider,a as useMDXComponents,i as withMDXComponents};
+//# sourceMappingURL=index-f9552162.js.map
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { marked } from "https://cdnjs.cloudflare.com/ajax/libs/marked/9.0.0/lib/marked.esm.js";
 
-class ToHtml extends HTMLElement { // (1)
+class MarkdownElement extends HTMLElement { // (1)
 
   connectedCallback() {
     this.innerHTML = marked(this.innerHTML);
@@ -8,4 +8,4 @@ class ToHtml extends HTMLElement { // (1)
 
 }
 
-customElements.define("to-html", ToHtml); // (2)
+customElements.define("data-markdown", MarkdownElement); // (2)

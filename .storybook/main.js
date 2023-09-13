d968be413761b2663887088ac197bd3542e24834
rhildred <rhildred@gmail.com>
2023-09-13 15:01:06 -0400

    doesn't seem to work with .md

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-    stories: ["../README.md", "../src/stories/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+    stories: ["../src/stories/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
     addons: [
       "@storybook/addon-links",
       "@storybook/addon-essentials",

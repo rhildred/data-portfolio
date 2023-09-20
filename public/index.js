25c41de11ec29e03399d228ac7238fe19c2c9acc
Richard Hildred <rhildred@gmail.com>
2023-09-20 15:27:51 -0400

    get attributes.src

--- public/index.js
+++ public/index.js
@@ -3,8 +3,8 @@ import { marked } from "https://cdnjs.cloudflare.com/ajax/libs/marked/9.0.0/lib/
 class MarkdownElement extends HTMLElement { // (1)
 
   async connectedCallback() {
-    if(this.src){
-      const res = await fetch(this.src);
+    if(this.attributes.src){
+      const res = await fetch(this.attributes.src);
       this.innerHtml = await res.text();
     }else{
       this.innerHTML = marked(this.innerHTML);

73fd80c5893f3ae2f0cb4ff40b586fd43ad48950
rhildred <rhildred@gmail.com>
2023-09-20 16:06:42 -0400

    old self = this trick

--- public/index.js
+++ public/index.js
@@ -4,11 +4,11 @@ class MarkdownElement extends HTMLElement { // (1)
 
   async connectedCallback() {
     if(this.attributes.src){
-      let oDest = this.innerHTML;
+      const self = this;
       const sUrl = this.attributes.src.baseURI + this.attributes.src.value;
       const res = await fetch(sUrl);
       const sText = await res.text();
-      oDest = sText;
+      self.innerHTML = sText;
     }else{
       this.innerHTML = marked(this.innerHTML);
     }
